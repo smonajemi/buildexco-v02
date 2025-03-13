355fea81e1e790128a55b2bb9697dc220972f3fe
codewithsenna <codewithsenna@gmail.com>
2025-03-12 22:43:39 -0400

    refactored

--- src/middlewares/cspMiddleware.js
+++ src/middlewares/cspMiddleware.js
@@ -2,8 +2,8 @@ const cspMiddleware = (req, res, next) => {
     res.setHeader(
       "Content-Security-Policy",
       "default-src 'self'; " +
-      "script-src 'self' https://code.jquery.com https://cdn.jsdelivr.net; " +
-      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://use.fontawesome.com; " +
+      "script-src 'self' https://code.jquery.com https://cdn.jsdelivr.net https://www.google.com https://www.gstatic.com;" +
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.googleapis.com https://fonts.gstatic.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://use.fontawesome.com; " +
       "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://use.fontawesome.com data:; " +
       "img-src 'self' data: https://www.buildexco.com; " +
       "object-src 'none'; " +
