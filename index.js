944d6332e2128b28bcfdb750f99d8ce821e1c168
codewithsenna <codewithsenna@gmail.com>
2025-03-26 16:57:58 -0400

    mongoDB connected

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import app from './src/app.js'
+import app from './src/app.js';
+import connectDB from './src/config/db.js'
 
 const PORT = process.env.PORT || 3000;
 let server;
 
 const startServer = async () => {
   try {
+    await connectDB();
+
     server = app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
